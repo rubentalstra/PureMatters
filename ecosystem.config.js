1c6a0c9a0069c826128b97133f3131b92231c086
Ruben Talstra <RubenTalstra1211@outlook.com>
2024-06-07 17:23:37 +0200

    update port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
             instances: '1',
             exec_mode: 'fork',
             env_localhost: {
-                PORT: 443,
+                PORT: 8443,
                 NODE_ENV: 'localhost',
             },
             env_production: {
-                PORT: 443,
+                PORT: 8443,
                 NODE_ENV: 'production',
             },
         },
